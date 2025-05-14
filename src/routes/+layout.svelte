2b36540f6bdf8665ae7fe2b80fad38e6d8249a70
spitkov <github@spitkov.hu>
2025-05-14 17:45:02 +0200

    og

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,11 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<meta property="og:title" content="Hogyan legyél ingyenélő" />
+	<meta property="og:description" content="Ingyen mobilnet tutorial" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://freemobilenetguide.github.io" />
+</svelte:head>
+
 {@render children()}
